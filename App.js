857f0f1663377ad49addfcffd95cd5b7855f8f2c
princeparvat80 <princeparvat80@gmail.com>
2024-06-12 09:23:04 +0530

    Functional Component

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import React from "react";
-import ReactDOM from "react-dom";
-//--------------------------------------------------------------------------------------------------------------
-//     This is how we create element by JavaScript
-//     const heading = document.createElement("h1");
-//     heading.innerHTML = "Prince Parvat";
-//     const root = document.getElementById("root");
-//     root.appendChild(heading);
-//--------------------------------------------------------------------------------------------------------------
+// // --------------------------------------------------------------------------------------------------------------
+// //     This is how we create element by JavaScript
+// //     const heading = document.createElement("h1");
+// //     heading.innerHTML = "Prince Parvat";
+// //     const root = document.getElementById("root");
+// //     root.appendChild(heading);
+// // --------------------------------------------------------------------------------------------------------------
 
-// This is how we create element by JavaScript
-const heading = React.createElement("h1", {}, "Learn"); //This object in this line will use to give the id to the elemnet.
-const heading2 = React.createElement("h2", {}, "Learn 2");
-const container = React.createElement("div", { id: "container" }, [
-  heading,
-  heading2,
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+// // This is how we create element by React
+// // const heading = React.createElement("h1", {}, "Learn"); //This object in this line will use to give the id to the elemnet.
+// // const heading2 = React.createElement("h2", {}, "Learn 2");
+// // const container = React.createElement("div", { id: "container" }, [
+// //   heading,
+// //   heading2,
+// // ]);
+// // --------------------------------------------------------------------------------------------------------------
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// This is how we create element by JSX
+const heading = (
+  <h1 id="h1" key="key1">
+    Learing JSX
+  </h1>
+);
+// const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+// rootElement.render(heading);
+
+// Functional Component
+const Title = () => (
+    <h1 id="h1" key="key1">
+      Learing JSX Title
+    </h1>
+  );
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      {heading}
+      <Title /> 
+      <h1>Functional component</h1>
+    </div>
+  );
+};
+
+const fucntionalComponentRoot = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+fucntionalComponentRoot.render(<HeaderComponent />);
